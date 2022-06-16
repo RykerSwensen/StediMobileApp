2fe410105cf2d10aff8d11bd51aab17528cf4926
FIRST_NAME LAST_Name <rykerswensen@gmail.com>
2022-06-16 15:00:43 -0600

    new

--- stedi-react-native-main/Login.js
+++ stedi-react-native-main/Login.js
@@ -1,9 +1,27 @@
 import { useState } from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const sendText = async (phoneNumber) => {
+  console.log("PhoneNumber: ", phoneNumber);
+  await fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber, {
+    method: "POST",
+    headers: {
+      "content-type": "application/text",
+    },
+  });
+};
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount((prevCount) => prevCount + 1);
 
   return (
     <SafeAreaView style={styles.margin}>
@@ -12,15 +30,34 @@ const Login = () => {
         onChangeText={setPhoneNumber}
         value={phoneNumber}
         placeholder="801-555-1212"
+        placeholderTextColor="#2d32c4"
       />
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="1234"
+        placeholderTextColor="#2d32c4"
         keyboardType="numeric"
         secureTextEntry={true}
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          console.log("Login Button was clicked");
+        }}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          sendText(phoneNumber);
+        }}
+      >
+        <Text>Press Here to get Text</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -34,6 +71,12 @@ const styles = StyleSheet.create({
   },
   margin: {
     marginTop: 100,
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "8c8888",
+    padding: 10,
   },
 });
 
